38ecb2952810b47869e75018b5e4a0f380102842
MinhTranNTT <tvhuuminhntt@gmail.com>
2022-09-11 13:14:10 +0700

    add country rest and setting with role

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Country.java
@@ -31,6 +31,12 @@ public class Country {
 		
 	}
 	
+	public Country(Integer id, String name, String code) {
+		this.id = id;
+		this.name = name;
+		this.code = code;
+	}
+	
 	public Country(String name, String code) {
 		this.name = name;
 		this.code = code;
@@ -60,13 +66,13 @@ public class Country {
 		this.code = code;
 	}
 
-	public Set<State> getStates() {
-		return states;
-	}
-
-	public void setStates(Set<State> states) {
-		this.states = states;
-	}
+//	public Set<State> getStates() {
+//		return states;
+//	}
+//
+//	public void setStates(Set<State> states) {
+//		this.states = states;
+//	}
 
 	@Override
 	public String toString() {
