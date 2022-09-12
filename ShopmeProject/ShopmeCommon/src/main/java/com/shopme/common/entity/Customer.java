e5f806b827b0fbd9a672eb70740ff23e3a87a9c6
MinhTranNTT <tvhuuminhntt@gmail.com>
2022-09-12 22:57:03 +0700

    code login authen #2

--- ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Customer.java
+++ ShopmeProject/ShopmeCommon/src/main/java/com/shopme/common/entity/Customer.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -57,6 +59,10 @@ public class Customer {
 	@Column(name = "created_time")
 	private Date createdTime;
 	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "authentication_type", length = 10)
+	private AuthenticationType authenticationType;
+	
 	@ManyToOne
 	@JoinColumn(name = "country_id")
 	private Country country;
@@ -98,6 +104,14 @@ public class Customer {
 
 	public String getLastName() {
 		return lastName;
+	}
+
+	public AuthenticationType getAuthenticationType() {
+		return authenticationType;
+	}
+
+	public void setAuthenticationType(AuthenticationType authenticationType) {
+		this.authenticationType = authenticationType;
 	}
 
 	public void setLastName(String lastName) {
